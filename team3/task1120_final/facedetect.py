160e93e5393ad6607051ae23bbf4b728fe023c01
Daw13 <49241708+Daw13@users.noreply.github.com>
2019-12-10 15:05:06 +0800

    Update facedetect.py

--- team3/task1120_final/facedetect.py
+++ team3/task1120_final/facedetect.py
@@ -1,5 +1,10 @@
 import numpy as np
 import cv2
+
+import serial
+import serial.tools.list_ports
+import time
+
 lenth=1024
 width=678
 cap = cv2.VideoCapture(0)
@@ -12,13 +17,19 @@ def drawface(x,y,w,h,img):
         a=255
         b=0
 #        print('l\n')
+#        ser.write(0)
+
     elif fc>mid+limit:
         a=0
         b=255
 #        print('r\n')
+#        ser.write(1)
     else:
         a=255
         b=255
+#        print('toss\n')
+#        ser.write(2)
+
     cv2.rectangle(img,(0,0),(x,y),(a,b,0),5)
     cv2.rectangle(img,(x,0),(x+w,y),(a,b,0),5)
     cv2.rectangle(img,(x+w,0),(lenth,y),(a,b,0),5)
@@ -27,6 +38,18 @@ def drawface(x,y,w,h,img):
     cv2.rectangle(img,(0,y+h),(x,width),(a,b,0),5)
     cv2.rectangle(img,(x,y+h),(x+w,width),(a,b,0),5)
     cv2.rectangle(img,(x+w,y+h),(lenth,width),(a,b,0),5)
+
+ports = list(serial.tools.list_ports.comports())
+print (ports)
+
+for p in ports:
+    print (p[1])
+    if "Uno" in p[1]:
+        ser=serial.Serial(port=p[0])
+    else :
+        print ("No Arduino Device was found connected to the computer")
+
+time.sleep(2)
 
 while(True):
     ret, frame = cap.read()
