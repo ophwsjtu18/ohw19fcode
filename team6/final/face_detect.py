801a3524c40078fc7614b2bc52d04d204bc9055c
daxixi <pspo-qfl@163.com>
2019-12-18 13:08:15 +0800

    ./

--- team6/final/face_detect.py
+++ team6/final/face_detect.py
@@ -34,22 +34,22 @@ lastx_d=0
 currentx_d=0
 shoot=0
 #MC
-mc=minecraft.Minecraft.create()
-pos=mc.player.getTilePos()
-pos0=[]
-pos0.append(pos.x)
-pos0.append(pos.y)
-pos0.append(pos.z)
-des=House([pos.x+20,pos.y,pos.z],mc,block.GOLD_BLOCK.id,block.GLASS.id)
-des.buildall()
+#mc=minecraft.Minecraft.create()
+#pos=mc.player.getTilePos()
+#pos0=[]
+#pos0.append(pos.x)
+#pos0.append(pos.y)
+#pos0.append(pos.z)
+#des=House([pos.x+20,pos.y,pos.z],mc,block.GOLD_BLOCK.id,block.GLASS.id)
+#des.buildall()
 
 ct=0
 while(True):
     ct+=1
     #到达目的地了吗
-    if(des.isInsideHouse()):
-        mc.postToChat("You win")
-        break
+    #if(des.isInsideHouse()):
+    #    mc.postToChat("You win")
+    #    break
     #人脸识别，一方面投石机追踪，一方面控制MC里面人到Destinatioin
     ret,img=cap.read()
     center=[img.shape[0]/2,img.shape[1]/2]
@@ -62,7 +62,8 @@ while(True):
     else:
         for (x,y,w,h) in faces:
             img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-            roi_color = img[y:y+h, x:x+w]        
+            roi_color = img[y:y+h, x:x+w]
+            cv2.imshow('img',img)
             x_d=x+w/2-325
             dis=(-0.88*w+220)
             angle=math.atan(x_d/dis)/3.1415926535897*180
@@ -73,8 +74,8 @@ while(True):
                 lastpos=currentpos
                 lastdis=currentdis
                 lastx_d=currentx_d
-            pos=mc.player.getTilePos()
-            mc.player.setTilePos([pos.x+(currentx_d-lastx_d)/5,pos.y,pos.z+(currentdis-lastdis)/5])
+            #pos=mc.player.getTilePos()
+            #mc.player.setTilePos([pos.x+(currentx_d-lastx_d)/5,pos.y,pos.z+(currentdis-lastdis)/5])
             #print(dis)
             #print(angle)
             #ser.write
@@ -82,19 +83,18 @@ while(True):
             #ser.write
             print(str(int(angle)).encode())
             #time.sleep(0.5)
-            cv2.imshow('img',img)
             if((lastpos-currentpos)<5 and abs(angle)<15):
                 shoot+=1
         if(shoot>10):
-            mc.player.setTilePos([0,-1000,0])
+            #mc.player.setTilePos([0,-1000,0])
             #ser.write("s".encode())
             time.sleep(2)
             shoot=0
         lastpos=currentpos
         lastdis=currentdis
         lastx_d=currentx_d
-        if cv2.waitKey(1)& 0xFF==ord('q'):
-            break
+    if cv2.waitKey(1)& 0xFF==ord('q'):
+        break
     
 
 cap.release()
